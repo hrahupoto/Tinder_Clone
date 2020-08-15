34697b190ba1ae49f71833d5f1d09290f47e258c
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-08-16 01:05:20 +1000

    Tinder clone signin, signup and user pages work, get requests work

--- public/env.js
+++ public/env.js
@@ -3,38 +3,49 @@ $(document).ready(function () {
   $("#login_btn").click(function () {
     var email = $("#email").val();
     var password = $("#password").val();
+
+    $.ajax({
+        type:'GET',
+        url:'/check_user',
+        data: {email,password},
+        success: function(data){
+            window.location.href = data;
+            if(data=="./index.html"){
+                alert("User not found!")
+            }
+        },
+        error:function(){
+            
+        }
+    });
     
-    let userDetails=JSON.parse(localStorage.getItem("userDetails"))
-    
- 
-    if (userDetails.signupEmail === email && userDetails.signupPassword === password) {
-      alert("Go Ahead!");
-    } else {
-        alert("Ooops");
-    }
+
+  
   });
-
 });
-
 
 $(document).ready(function () {
   // Get value on button click and show alert
   $("#signup_btn").click(function () {
+    let userDetails = {
+      firstName: $("#first_name").val(),
+      lastName: $("#last_name").val(),
+      signupEmail: $("#signup_email").val(),
+      signupPassword: $("#signup_password").val(),
+    };
 
-    let userDetails={
-        firstName: $("#first_name").val(),
-        lastName: $("#last_name").val(),
-        signupEmail: $("#signup_email").val(),
-        signupPassword: $("#signup_password").val(),
-            
-    }
 
-    
-
-    localStorage.setItem("userDetails",JSON.stringify(userDetails))
-    
-   
-    window.location.href= "./index.html"
-    
+    $.ajax({
+        type:'GET',
+        url:'/save_userdetails',
+        data: userDetails,
+        success: function(newdata){
+            alert(newdata)
+        },
+        error:function(){
+            alert('error saving data')
+        }
+    });
+    window.location.href = "./index.html";
   });
 });
--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const { request } = require('express');
 const app = express();
-
-
-
+var userDetails;
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/signup', function (req, res) {
-//     let userdetails = req.query.userdetails;
-//     console.log(userdetails);
-//     //res.send('Hello World')
-// })
+app.get('/save_userdetails', function (req, res) {
+    userDetails={
+        firstName: req.query.firstName,
+        lastName: req.query.lastName,
+        signupEmail: req.query.signupEmail,
+        signupPassword: req.query.signupPassword,
+        }
 
+})
 
+app.get('/check_user', function (req, res) {
+    var email=req.query.email;
+    var password=req.query.password;
+
+    try {
+        if (userDetails.signupEmail === email && userDetails.signupPassword === password) {
+            res.send("./user.html");
+        } 
+        else 
+        {
+          res.send("./index.html");
+        }    
+    } catch (error) {
+        console.log("error caught");
+    }
+    
+})
 
 
 app.listen(3000);
