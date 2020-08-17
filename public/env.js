de2a844a754cd5823fc33d6ef8cf81854e17cb07
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-08-17 23:19:52 +1000

    -Connected to mongodb -Swipe feat works -Create new users works -User profile picture feat added -getting user profile details from db works

--- public/env.js
+++ public/env.js
@@ -3,24 +3,27 @@ $(document).ready(function () {
   $("#login_btn").click(function () {
     var email = $("#email").val();
     var password = $("#password").val();
+    
+    var img = $('<img id="dynamic">');
+    img.attr("src", "./images/1-intro-photo-final.jpg");
+    img.appendTo("#content");
 
     $.ajax({
-        type:'GET',
-        url:'/check_user',
-        data: {email,password},
-        success: function(data){
-            window.location.href = data;
-            if(data=="./index.html"){
-                alert("User not found!")
-            }
-        },
-        error:function(){
-            
+      type: "GET",
+      url: "/check_user",
+      data: { email, password },
+      success: function (data) {
+        if (data == "./index.html") {
+          alert("Incorrect email or password");
+        } else if (data == "User does not exist!") {
+          alert("User does not exist!");
+        } else {
+          window.location.href = data;
+          sessionStorage.setItem("user_Session", "logged-in");
         }
+      },
+      error: function () {},
     });
-    
-
-  
   });
 });
 
@@ -34,18 +37,89 @@ $(document).ready(function () {
       signupPassword: $("#signup_password").val(),
     };
 
-
     $.ajax({
-        type:'GET',
-        url:'/save_userdetails',
-        data: userDetails,
-        success: function(newdata){
-            alert(newdata)
-        },
-        error:function(){
-            alert('error saving data')
-        }
+      type: "GET",
+      url: "/save_userdetails",
+      data: userDetails,
+      success: function (newdata) {
+        alert(newdata);
+      },
+      error: function () {
+        alert("error saving data");
+      },
     });
     window.location.href = "./index.html";
+  });
+});
+
+$(document).ready(function () {
+  $("#profile").click(function () {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("userdetails").style.display = "block";
+    document.getElementById("profile_photo").style.display = "block";
+
+    $("#userdetails").html(
+      "<ul class='collection'><li id='item1' class='collection-item'></li><li id='item2' class='collection-item'></li><li id='item3' class='collection-item'></li></ul>"
+    );
+
+    $.ajax({
+      type: "GET",
+      url: "/userdetails",
+      success: function (data) {
+        document.getElementById("item1").innerHTML =
+          "First Name: " + data.firstName;
+        document.getElementById("item2").innerHTML =
+          "Last Name: " + data.lastName;
+        document.getElementById("item3").innerHTML =
+          "Email: " + data.signupEmail;
+      },
+    });
+  });
+});
+
+$(document).ready(function () {
+  $("#matches").click(function () {
+    document.getElementById("content").style.display = "block";
+    document.getElementById("userdetails").style.display = "none";
+    document.getElementById("profile_photo").style.display = "none";
+  });
+});
+
+$(document).ready(function () {
+  $("#content").draggable({ revert: "valid" });
+
+  $("#droppable_left").droppable({
+    classes: {
+      "ui-droppable-active": "ui-state-active",
+      "ui-droppable-hover": "ui-state-hover",
+    },
+    drop: function (event, ui) {
+      $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+      $("#content > img").remove();
+
+      setTimeout(function () {
+        var img = $('<img id="dynamic">');
+        img.attr("src", "./images/photo.jpeg");
+
+        img.appendTo("#content");
+      }, 500);
+    },
+  });
+
+  $("#droppable_right").droppable({
+    classes: {
+      "ui-droppable-active": "ui-state-active",
+      "ui-droppable-hover": "ui-state-hover",
+    },
+    drop: function (event, ui) {
+      $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+      $("#content > img").remove();
+
+      setTimeout(function () {
+        var img = $('<img id="dynamic">');
+        img.attr("src", "./images/photo.jpeg");
+        img.appendTo("#content");
+      }, 500);
+    },
   });
 });
--- server.js
+++ server.js
@@ -1,37 +1,67 @@
-const express = require('express');
-const { request } = require('express');
+const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const { request } = require("express");
 const app = express();
 var userDetails;
-app.use(express.static(__dirname + '/public'));
+const uri =
+  "mongodb+srv://Hassan:SIT725@sit725.bketa.mongodb.net/SIT725?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+app.use(express.static(__dirname + "/public"));
 
-app.get('/save_userdetails', function (req, res) {
-    userDetails={
-        firstName: req.query.firstName,
-        lastName: req.query.lastName,
-        signupEmail: req.query.signupEmail,
-        signupPassword: req.query.signupPassword,
-        }
+app.get("/save_userdetails", function (req, res) {
+  
 
-})
+  client.connect((err) => {
+    const collection = client.db("SIT725").collection("userdetails");
+    collection.insertOne({ 
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    signupEmail: req.query.signupEmail,
+    signupPassword: req.query.signupPassword,
+    });
+   // client.close();
+  });
+});
 
-app.get('/check_user', function (req, res) {
-    var email=req.query.email;
-    var password=req.query.password;
+app.get("/check_user", function (req, res) {
+  var email = req.query.email;
+  var password = req.query.password;
 
-    try {
-        if (userDetails.signupEmail === email && userDetails.signupPassword === password) {
-            res.send("./user.html");
-        } 
-        else 
-        {
-          res.send("./index.html");
-        }    
-    } catch (error) {
-        console.log("error caught");
-    }
+  client.connect((err) => {
+    var data = client
+      .db("SIT725")
+      .collection("userdetails")
+      .find().toArray(function(err,result){
+        result.filter(function(data){
+
+            try {
+                if (
+                  data.signupEmail === email &&
+                  data.signupPassword === password
+                ) {
+                  userDetails=data;  
+                  res.send("./user.html");
+                } else {
+                  res.send("./index.html");
+                }
+                //client.close();
+              } catch (error) {
+                res.send("User does not exist!");
+                
+              }
+        })
+          
+      })
+      
     
-})
+  });
+  //client.close();
+  
+});
 
+app.get("/userdetails", function (req, res) {
+  res.send(userDetails);
+});
 
 app.listen(3000);
 console.log("Server running at Port: 3000");
